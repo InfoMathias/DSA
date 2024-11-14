fc9ed0d09c774b1d413e21d5132d5bc0debff3d2
Mathias Magras <43543036+InfoMathias@users.noreply.github.com>
2024-11-14 09:11:12 -0400

    Create leetcode-724-find-pivot-index.py

---
+++ leetcode-724-find-pivot-index.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/find-pivot-index/
+
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        leftSum, rightSum = 0, sum(nums)
+        for idx, ele in enumerate(nums):
+            rightSum -= ele
+            if leftSum == rightSum:
+                return idx
+            leftSum += ele
+        return -1
