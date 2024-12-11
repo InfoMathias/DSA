089dbc11466ea719ab71a7b1384056609fcb0750
Mathias Magras <43543036+InfoMathias@users.noreply.github.com>
2024-12-10 23:05:53 -0400

    Create leetcode-1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py

---
+++ leetcode-1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/
+
+class Solution:
+    def dfs(self, adj: List[List[Tuple[int, int]]], visited: List[bool], minChange: List[int], currCity: int) -> None:
+        visited[currCity] = True
+        for neighbourCity in adj[currCity]:
+            if not visited[neighbourCity[0]]:
+                if neighbourCity[1] == 1:
+                    minChange[0] += 1
+                self.dfs(adj, visited, minChange, neighbourCity[0])
+    
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        adj = [[] for _ in range(n)]
+        for connection in connections:
+            adj[connection[0]].append((connection[1], 1))
+            adj[connection[1]].append((connection[0], -1))
+        visited = [False] * n
+        minChange = [0]
+        self.dfs(adj, visited, minChange, 0)
+        return minChange[0]
